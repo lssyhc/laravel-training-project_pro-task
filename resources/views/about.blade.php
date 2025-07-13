cf021aaf011524710ee84aaf0abd1285b5c42ab4
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-13 19:28:40 +0700

    feat: add about page and route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'About Page')
+@section('content')
+    <div class="flex h-screen items-center justify-center">
+        <h1 class="text-center text-4xl font-bold">This is About Page</h1>
+    </div>
+@endsection
